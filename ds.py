9a571a192af7b76b09f32944b9eac63c10f378d0
Dharmendra Tolani <dhtolani@microsoft.com>
2017-07-03 15:48:33 +0530

    shuffle the data

--- ds.py
+++ ds.py
@@ -4,11 +4,13 @@ from sklearn import svm, metrics
 csv = np.genfromtxt ('Train.csv', delimiter=",")
 csv =  np.delete(csv,0,0) #delete header row
 csv =  np.delete(csv,0,1) #delete id column
+np.random.shuffle(csv)
 labels = csv[:,256] #labels
 csv =  np.delete(csv,256,axis=1) #delete labels column
 print(csv.shape)
 print(csv)
 print(labels)
+print(sum(labels))
 print(np.unique(labels))
 
 n_samples = len(csv)
@@ -21,3 +23,19 @@ print("Classification report for classifier %s:\n%s\n"
       % (classifier, metrics.classification_report(expected, predicted)))
 print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
 
+# commit id 2604ec9
+# Classification report for classifier SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+#   decision_function_shape=None, degree=3, gamma=0.001, kernel='rbf',
+#   max_iter=-1, probability=False, random_state=None, shrinking=True,
+#   tol=0.001, verbose=False):
+#              precision    recall  f1-score   support
+#
+#         0.0       0.96      0.29      0.45      2336
+#         1.0       0.85      1.00      0.92      9410
+#
+# avg / total       0.87      0.86      0.82     11746
+#
+#
+# Confusion matrix:
+# [[ 682 1654]
+#  [  30 9380]]
